d783ed4a9252017110744c019d53240950a925eb
Cmonfreeman <130077814+CmonFreeman@users.noreply.github.com>
2023-11-13 17:07:06 -0500

    Wrote getComputerChoice function and tested

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,12 @@
-function getComputerChoice
 //get player selection
 //check that player selection matches criteria
 //get computer selection
+const choices = ["Rock", "Paper", "Scissors"];
+function getComputerChoice() {
+    let shoot = choices[Math.floor(Math.random() * choices.length)];
+    return shoot;
+}
+
+getComputerChoice();
 //compare computer/player selection
 //return winner
